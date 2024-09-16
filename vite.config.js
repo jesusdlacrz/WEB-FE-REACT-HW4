94239b639f4c5ec51f3d988b471c8eedf479147a
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-09-16 08:54:13 -0500

    Add predeploy script and update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WEB-FE-REACT-HW4/'
+  base: 'https://jesusdlacrz.github.io/WEB-FE-REACT-HW4/'
 })
